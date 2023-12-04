08d73f065fae278c36bae4fce6a0d9af553b7182
Pierre Prudhomme <prudh.pierre@gmail.com>
2023-12-04 17:53:18 +0100

    Bouton add

--- app/src/main/java/com/pp/todopierre/list/TaskListFragment.kt
+++ app/src/main/java/com/pp/todopierre/list/TaskListFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.pp.todopierre.R
+import java.util.UUID
 
 class TaskListFragment : Fragment() {
 
@@ -34,5 +36,13 @@ class TaskListFragment : Fragment() {
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = adapter
+
+        view.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener{
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList = taskList + newTask
+            adapter.currentList = taskList
+            adapter.notifyDataSetChanged()
+        }
+
     }
 }
